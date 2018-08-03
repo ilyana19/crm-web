326f088496759430fd2be38412a34d3d3ee12997
ilyana19 <aries_2152@hotmail.com>
2018-08-03 14:46:41 -0400

    fixed some things with erb routing and can now edit a contact

--- server.rb
+++ server.rb
@@ -49,6 +49,22 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+
+  if @contact
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      note: params[:note]
+    )
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/about' do
   erb :about
 end
